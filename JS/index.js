91603932d6d4aabbb90a6d4ce031ffb3f0e0d4d4
Guimeb <rm551981@fiap.com.br>
2023-05-19 23:57:15 -0300

    merge branch main

--- JS/index.js
+++ JS/index.js
@@ -1,89 +1,116 @@
-//sistema de formulario e validaçao de conta
-var cidades = document.getElementById("cidades"); 
-var precipitacao = document.getElementById("precipitacao"); 
-
-function validacao(event) { 
-    event.preventDefault();
-  var email = document.getElementById("email").value;
-  var senha = document.getElementById("senha").value;
-
-  if (email == "contatogalofiap@gmail.com" && senha == "12345") {
-    var cidades = document.getElementById("cidades");
-    var precipitacao = document.getElementById("precipitacao");
-    cidades.removeAttribute("disabled");
-    precipitacao.removeAttribute("disabled");
+//Variaveis
+const loginBar = document.querySelector('.loginBar')
+const loginNav = document.querySelector('#Login')
+//Verificação se o usuario esta logado
+window.addEventListener("load", ()=>{
+  console.log("Evento de carregamento acionado");
+  let userLog = JSON.parse(localStorage.getItem("userLog"));
+  console.log(userLog)
+  if (userLog === 1) {
+    loginNav.href = 'javascript:void(0);'
+    console.log("alo");
+    let userLogado = JSON.parse(localStorage.getItem('logedUser'));
+    console.log(userLogado);
+    console.log(userLogado[0].name);
+    loginNav.innerHTML = userLogado[0].name
+    loginNav.addEventListener('click', () => {
+      loginBar.classList.toggle('open');
+    });
+    loginBar.addEventListener('click', ()=>{
+      localStorage.setItem("userLog", JSON.stringify("0"))
+      location.reload();
+      console.log('teste');
+    })
+  } else {
+    console.log("whats wrong?")// alterar para usuario deslogado
   }
-}
+})
 
-function selectCidades() {
-    var c1 = document.getElementById("cidades");
-    var capacidade = document.getElementById("capacidade");
-    var CS = c1.options[c1.selectedIndex].value;
+// //sistema de formulario e validaçao de conta
+// var cidades = document.getElementById("cidades"); 
+// var precipitacao = document.getElementById("precipitacao"); 
+
+// function validacao(event) { 
+//     event.preventDefault();
+
+
+//   if (email == "contatogalofiap@gmail.com" && senha == "12345") {
+//     var cidades = document.getElementById("cidades");
+//     var precipitacao = document.getElementById("precipitacao");
+//     cidades.removeAttribute("disabled");
+//     precipitacao.removeAttribute("disabled");
+//   }
+// }
+
+// function selectCidades() {
+//     var c1 = document.getElementById("cidades");
+//     var capacidade = document.getElementById("capacidade");
+//     var CS = c1.options[c1.selectedIndex].value;
   
-    if (CS == "op1") {
-      capacidade.innerHTML = "Capacidade: 10mm de chuva";
-    } else if (CS == "op2") {
-      capacidade.innerHTML = "Capacidade: 15mm";
-    } else if (CS == "op3") {
-        capacidade.innerHTML = "Capacidade: 20mm";
-      } else if (CS == "op4") {
-        capacidade.innerHTML = "Capacidade: 30mm";
-    } else {
-      capacidade.innerHTML = "Capacidade:";
-    }
-  }
+//     if (CS == "op1") {
+//       capacidade.innerHTML = "Capacidade: 10mm de chuva";
+//     } else if (CS == "op2") {
+//       capacidade.innerHTML = "Capacidade: 15mm";
+//     } else if (CS == "op3") {
+//         capacidade.innerHTML = "Capacidade: 20mm";
+//       } else if (CS == "op4") {
+//         capacidade.innerHTML = "Capacidade: 30mm";
+//     } else {
+//       capacidade.innerHTML = "Capacidade:";
+//     }
+//   }
   
 
-function galoWeather(event) {
-  event.preventDefault();
-  var c1 = document.getElementById("cidades");
-  var CS = c1.options[c1.selectedIndex].value;
-  var PS = document.getElementById("precipitacao").value;
-  var resultado = document.getElementById("resultado");
+// function galoWeather(event) {
+//   event.preventDefault();
+//   var c1 = document.getElementById("cidades");
+//   var CS = c1.options[c1.selectedIndex].value;
+//   var PS = document.getElementById("precipitacao").value;
+//   var resultado = document.getElementById("resultado");
 
-  if (CS === "op1") {
-    if (PS > 10) {
-      resultado.innerHTML = "Vai alagar na Mocca, principalmente no centro e extremidades.";
-      console.log("Vai alagar na Mocca, principalmente no centro e extremidades.");
-    }else if(PS > 5 && PS <= 10){
-        resultado.innerHTML = "Nao vai alagar na Mocca, mas algum lugares podem enfrentar transito";
-        console.log("Nao vai alagar na Mocca, mas algum lugares podem enfrentar transito");
-      } else {
-      resultado.innerHTML = "Não vai alagar na Mocca";
-      console.log("Não vai alagar na Mocca");
-    }
-  } else if (CS === "op2") {
-    if (PS > 15) {
-      resultado.innerHTML = "Vai alagar na Vila Prudente, areas residenciais podem ser prejudicadas.";
-      console.log("Vai alagar na Vila Prudente, areas residenciais podem ser prejudicadas.");
-    }else if(PS > 10 && PS <= 15){
-      resultado.innerHTML = "Nao vai alagar na Vila Prudente, mas algumas casas podem ser prejudicadas com muita agua";
-      console.log("Nao vai alagar na Vila Prudente, mas algumas casas podem ser prejudicadas com muita agua");
-    } else {
-      resultado.innerHTML = "Não vai alagar na Vila Prudente";
-      console.log("Não vai alagar na Vila Prudente");
-    } 
-} else if (CS === "op3") {
-    if (PS > 20) {
-      resultado.innerHTML = "Vai alagar no Tatuapé, Avenidas principais ficaram obstruidas.";
-      console.log("Vai alagar no Tatuapé, Avenidas principais ficaram obstruidas.");
-    }else if(PS > 10 && PS <= 20){
-      resultado.innerHTML = "Nao vai alagar no Tatuapé, mas algum lugares podem ter problemas com transito";
-      console.log("Nao vai alagar no Tatuapé, mas algum lugares podem ter problemas com transito");
-    } else {
-    resultado.innerHTML = "Não vai alagar no Tatuapé";
-    console.log("Não vai alagar no Tatuapé");
-    }
-  } else if (CS === "op4") {
-    if (PS > 30) {
-      resultado.innerHTML = "Vai alagar em Belenzinho, areas residenciais podem ser prejudicadas";
-      console.log("Vai alagar em Belenzinho, areas residenciais podem ser prejudicadas");
-    }else if(PS > 15 && PS <= 30){
-      resultado.innerHTML = "Nao vai alagar em Belenzinho, mas algumas casas podem enfrentar perda de energia."
-      console.log("Nao vai alagar em Belenzinho, mas algumas casas podem enfrentar perda de energia.");
-    } else {
-      resultado.innerHTML = "Não vai alagar em Belenzinho";
-      console.log("Não vai alagar em Belenzinho");
-    }
-  }
-}
+//   if (CS === "op1") {
+//     if (PS > 10) {
+//       resultado.innerHTML = "Vai alagar na Mocca, principalmente no centro e extremidades.";
+//       console.log("Vai alagar na Mocca, principalmente no centro e extremidades.");
+//     }else if(PS > 5 && PS <= 10){
+//         resultado.innerHTML = "Nao vai alagar na Mocca, mas algum lugares podem enfrentar transito";
+//         console.log("Nao vai alagar na Mocca, mas algum lugares podem enfrentar transito");
+//       } else {
+//       resultado.innerHTML = "Não vai alagar na Mocca";
+//       console.log("Não vai alagar na Mocca");
+//     }
+//   } else if (CS === "op2") {
+//     if (PS > 15) {
+//       resultado.innerHTML = "Vai alagar na Vila Prudente, areas residenciais podem ser prejudicadas.";
+//       console.log("Vai alagar na Vila Prudente, areas residenciais podem ser prejudicadas.");
+//     }else if(PS > 10 && PS <= 15){
+//       resultado.innerHTML = "Nao vai alagar na Vila Prudente, mas algumas casas podem ser prejudicadas com muita agua";
+//       console.log("Nao vai alagar na Vila Prudente, mas algumas casas podem ser prejudicadas com muita agua");
+//     } else {
+//       resultado.innerHTML = "Não vai alagar na Vila Prudente";
+//       console.log("Não vai alagar na Vila Prudente");
+//     } 
+// } else if (CS === "op3") {
+//     if (PS > 20) {
+//       resultado.innerHTML = "Vai alagar no Tatuapé, Avenidas principais ficaram obstruidas.";
+//       console.log("Vai alagar no Tatuapé, Avenidas principais ficaram obstruidas.");
+//     }else if(PS > 10 && PS <= 20){
+//       resultado.innerHTML = "Nao vai alagar no Tatuapé, mas algum lugares podem ter problemas com transito";
+//       console.log("Nao vai alagar no Tatuapé, mas algum lugares podem ter problemas com transito");
+//     } else {
+//     resultado.innerHTML = "Não vai alagar no Tatuapé";
+//     console.log("Não vai alagar no Tatuapé");
+//     }
+//   } else if (CS === "op4") {
+//     if (PS > 30) {
+//       resultado.innerHTML = "Vai alagar em Belenzinho, areas residenciais podem ser prejudicadas";
+//       console.log("Vai alagar em Belenzinho, areas residenciais podem ser prejudicadas");
+//     }else if(PS > 15 && PS <= 30){
+//       resultado.innerHTML = "Nao vai alagar em Belenzinho, mas algumas casas podem enfrentar perda de energia."
+//       console.log("Nao vai alagar em Belenzinho, mas algumas casas podem enfrentar perda de energia.");
+//     } else {
+//       resultado.innerHTML = "Não vai alagar em Belenzinho";
+//       console.log("Não vai alagar em Belenzinho");
+//     }
+//   }
+// }
---
+++ JS/login.js
@@ -0,0 +1,120 @@
+//Variaveis
+const loginIndex = document.querySelector('#sign-up')
+const cadastroCard = document.querySelector('.cadastro-container')
+const cadastroIndex = document.querySelector('#register')
+const loginClick = document.querySelector("#login-submit-btn")
+const cadastroClick = document.querySelector("#cadastro-submit-btn")
+const msgError = document.querySelector('.errorMsg')
+const msgErrorC = document.querySelector('.errorMsgC')
+//Sistema de contas
+const user1 = {  //admin
+    name: "GaloAdmin",
+    emailUser:"contatogalofiap@gmail.com",
+    senhaUser:"12345"
+}
+
+let userList = []
+userList.push(user1)
+
+//Local storage para usuario
+localStorage.setItem("userLog", JSON.stringify("0")) //define usuario deslogado
+localStorage.setItem("usersList", JSON.stringify(userList)) //define a um localStorage todos os usuarios existentes
+
+//Login
+loginClick.addEventListener("click", (login)=>{
+  login.preventDefault() //previne recarregamento da tela
+  let listaDeUsuario = JSON.parse(localStorage.getItem("usersList")); //busca contas no storage
+  let emailInput = document.querySelector("#email").value;
+  let senhaInput = document.querySelector("#senha").value;
+  if(emailInput === "" || senhaInput ===""){ //validações
+    msgError.innerHTML = 'Preencha todos os campos'
+    msgError.setAttribute('style', 'visibility: visible;')
+  }else{
+    const acharUsuario = listaDeUsuario.find(usuario => {
+      return usuario.emailUser === emailInput && usuario.senhaUser === senhaInput;
+    });
+    if(acharUsuario){
+      localStorage.setItem("userLog", "1");//define se o usuario foi logado
+      let logUser = [acharUsuario]; //define qual usuario foi logado
+      localStorage.setItem("logedUser", JSON.stringify(logUser))
+      window.location.href = "./index.html";
+      console.log("Usuario Validado!")
+    }else{
+      msgError.setAttribute('style', 'visibility: visible;')
+      msgError.innerHTML = 'Email ou senha incorretos'
+      console.log("Usuario Invalido digite novamente")
+    }
+  }
+})
+
+//Sistema para alterar entre cadastro e login
+cadastroIndex.addEventListener("click", ()=>{
+  cadastroCard.setAttribute('style', 'z-index: 2;')
+})
+loginIndex.addEventListener("click", ()=>{
+  cadastroCard.setAttribute('style', 'z-index: -2;')
+})
+
+//Cadastro
+cadastroClick.addEventListener("click", (cadastro)=>{
+  cadastro.preventDefault()
+  let nameInput = document.querySelector('#nameC').value;
+  let emailInput = document.querySelector('#emailC').value;
+  let senhaInput = document.querySelector('#senhaC').value;
+  let senhaConfirmInput = document.querySelector('#senhaCC').value;
+  // let regiaoInput = document.querySelector('#regiaoC').value;
+  if(nameInput == "" || emailInput == "" || senhaInput == "" || senhaConfirmInput === ""){
+    msgErrorC.innerHTML = 'Todos os campos devem ser preenchidos'
+    console.log('Todos os campos devem ser preenchidos');
+    msgErrorC.setAttribute('style', 'visibility: visible;')
+    return;
+  }
+  //email
+  if(emailInput.length < 5){
+    console.log('email com menos de 5 letras');
+    msgErrorC.innerHTML = 'Email deve conter mais de 5 caracteres'
+    msgErrorC.setAttribute('style', 'visibility: visible;')
+    return;
+  }else if(emailInput.indexOf('@') === -1){
+    console.log('email invalido nao contem "@"');
+    msgErrorC.innerHTML = 'Email deve conter "@"'
+    msgErrorC.setAttribute('style', 'visibility: visible;')
+    return;
+  }
+  //senha
+  if(senhaInput.length < 5){
+    console.log('senha deve conter mais de 5 digitos');
+    msgErrorC.innerHTML = 'Senha deve conter no minimo 5 digitos'
+    msgErrorC.setAttribute('style', 'visibility: visible;')
+    return;
+  }
+  //confirmar senha
+  if (senhaConfirmInput != senhaInput){
+    msgErrorC.innerHTML = 'Senhas digitadas não coincidem'
+    msgErrorC.setAttribute('style', 'visibility: visible;')
+    console.log('senhas nao coincidem')
+    return;
+  }
+  console.log('to funcionando');
+  msgErrorC.innerHTML = 'Cadastro efetuado com sucesso!';
+  msgErrorC.setAttribute('style', 'visibility: visible; color: #46f548;');
+  let nameInputCLEAR = document.querySelector('#nameC')
+  let emailInputCLEAR = document.querySelector('#emailC')
+  let senhaInputCLEAR = document.querySelector('#senhaC')
+  let senhaConfirmInputCLEAR = document.querySelector('#senhaCC')
+  nameInputCLEAR.value = ""
+  emailInputCLEAR.value = ""
+  senhaInputCLEAR.value = ""
+  senhaConfirmInputCLEAR.value = ""
+  const newUser ={
+    name: nameInput,
+    emailUser: emailInput,
+    senhaUser: senhaInput,
+  }
+  let listaDeUsuarioCadastro = JSON.parse(localStorage.getItem("usersList"));
+  listaDeUsuarioCadastro.push(newUser);
+  localStorage.setItem("usersList", JSON.stringify(listaDeUsuarioCadastro));
+  console.log(newUser)
+  userList.push(newUser)
+  console.log(userList)
+})
